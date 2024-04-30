ce24fbe6b0d379f70116a515d0451113d66936b6
SpencerCake <spencer@orqafpv.com>
2024-04-30 11:10:40 +0200

    Updated ORQAF405_SOC target

--- src/main/target/ORQAF405_SOC/target.h
+++ src/main/target/ORQAF405_SOC/target.h
@@ -53,15 +53,11 @@
 #define UART3_RX_PIN            PB11
 #define UART3_TX_PIN            PB10
 
-#define USE_UART5
-#define UART5_RX_PIN            PD2
-#define UART5_TX_PIN            PC12
-
 #define USE_UART6
 #define UART6_RX_PIN            PC7
 #define UART6_TX_PIN            PC6
 
-#define SERIAL_PORT_COUNT       5
+#define SERIAL_PORT_COUNT       4
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_GHST
